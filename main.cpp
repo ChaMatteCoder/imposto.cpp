2675160b7aa4648cc5d0ed8d08e2827d86dcf01d
Matheus Silva <matffernandesif@gmail.com>
2020-05-12 08:25:06 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+ double ganhoSalario, ganhoServicos, ganhoCapital, gastoMedico, gastoEducacao;
+
+ cout << "Renda anual com salario: ";
+ cin >> ganhoSalario;
+ cout << "Renda anual com prestacao de servico: ";
+ cin >> ganhoServicos;
+ cout << "Renda anual com ganho de capital: ";
+ cin >> ganhoCapital;
+ cout << "Gastos Medicos: ";
+ cin >> gastoMedico;
+ cout << "Gastos educacionais: ";
+ cin >> gastoEducacao;
+
     return 0;
 }
